06c94205ee773784dc2cfa6972d9f5de0ead6198
Samoyed-02 <gus02031365@gmail.com>
2025-10-26 21:49:53 +0900

    [Application] main 단순화 및 컨트롤러 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.service.CarRacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        CarRacingService carRacingService = new CarRacingService(outputView);
+        CarController carController = new CarController(inputView,carRacingService);
+        carController.preparationRace();
     }
 }
